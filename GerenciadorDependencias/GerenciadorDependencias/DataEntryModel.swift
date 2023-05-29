7e646f2654b392bede38e75b15f5fcc2437f47d3
André Toti <detoti@gmail.com>
2023-05-29 13:06:24 -0300

    First commit - Gerenciadores de Dependencias para iOS

---
+++ GerenciadorDependencias/GerenciadorDependencias/DataEntryModel.swift
@@ -0,0 +1,14 @@
+//
+//  DataEntryModel.swift
+//  GerenciadorDependencias
+//
+//  Created by André Toti on 29/05/23.
+//
+
+import Foundation
+import Charts
+
+struct DataEntryModel: Codable {
+    var x: Double
+    var y: Double
+}
